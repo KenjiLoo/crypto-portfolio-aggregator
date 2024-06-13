5728872d57bad090b9edc14687e7bbdaccd5553f
KenjiLoo <kenji@skyez.net>
2024-06-13 12:06:01 +0800

    delete uneccesary stuff

--- app/Http/Controllers/Api/SiteGroup/BaseController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\Api\SiteGroup;
-
-use App\Http\Controllers\Api\BaseApiController;
-
-abstract class BaseController extends BaseApiController
-{
-}
--- database/seeders/AdminRelatedSeeder.php
+++ database/seeders/AdminRelatedSeeder.php
@@ -26,31 +26,5 @@ class AdminRelatedSeeder extends Seeder
                 'updated_by'       => 1,
             ],
         ]);
-
-        DB::table('admin_profile')->truncate();
-        DB::table('admin_profile')->insert([
-            [
-                'name'          => 'Super Admin',
-                'is_superadmin' => true,
-                'updated_by'    => 1,
-            ],
-        ]);
-
-        DB::table('admin_profile_module')->truncate();
-        DBHelper::query("INSERT INTO admin_profile_module (admin_profile_id, module_id) SELECT 1, id FROM module WHERE type = 'admin'");
-
-        DB::table('admin_module')->truncate();
-        DB::table('admin_module')->insert([
-            [
-                'admin_id'  => 1,
-                'module_id' => 2,
-                'status'    => 'add'
-            ],
-            [
-                'admin_id'  => 1,
-                'module_id' => 7,
-                'status'    => 'remove'
-            ],
-        ]);
     }
 }
